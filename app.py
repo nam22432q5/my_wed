f599134dd9ae38db2f54f49088f2117dbcb88651
nam22432q5 <nguyenvunam28062005@gmail.com>
2026-04-20 20:34:41 +0700

    home.html

--- app.py
+++ app.py
@@ -12,4 +12,4 @@ def assets(filename):
     return send_from_directory('assets', filename)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=8000)
