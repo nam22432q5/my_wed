c377973b51683b7453292f9396af02b8fbd3eca3
nam22432q5 <nguyenvunam28062005@gmail.com>
2026-04-20 13:45:20 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, send_from_directory
+import os
+
+app = Flask(__name__, static_folder='.')
+
+@app.route('/')
+def index():
+    return send_from_directory('.', 'home.html')
+
+@app.route('/assets/<path:filename>')
+def assets(filename):
+    return send_from_directory('assets', filename)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
